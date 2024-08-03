78fdfcbfa34c92a4ecd85e4966da9513839ea348
Remco Goyvaerts <remco.goyvaerts@acagroup.be>
2024-08-03 16:11:57 +0200

    Update settle location move

--- src/app/game/moves.ts
+++ src/app/game/moves.ts
@@ -1,14 +1,21 @@
 import { GameState } from "@/types/game";
+import { INVALID_MOVE } from 'boardgame.io/core';
 
-export function PlaceSettlement({G, playerID}: {G: GameState, playerID: any}, rowId: number, colId: number) {
-    console.log(`Player ${playerID} is settling on ${rowId},${colId}`);
+export function PlaceSettlement({ G, playerID }: { G: GameState, playerID: any }, coords: number[][]) {
+    const settleSpot = G.settleSpots.find(spot => JSON.stringify(spot.coords) === JSON.stringify(coords));
+
+    if (settleSpot && settleSpot.playerId === null) {
+        settleSpot.playerId = playerID;
+    } else {
+        return INVALID_MOVE
+    }
 }
 
-export function PlaceRoad({G, playerID}: {G: GameState, playerID: any}) {
+export function PlaceRoad({ G, playerID }: { G: GameState, playerID: any }) {
     console.log(`Player ${playerID} is building a road`);
 }
 
-export function PlaceRobber({G, playerID}: {G: GameState, playerID: any}, tileId: number) {
+export function PlaceRobber({ G, playerID }: { G: GameState, playerID: any }, tileId: number) {
     const oldRobberTile = G.tiles.find(tile => tile.hasRobber);
     if (oldRobberTile) {
         const oldRobberIndex = G.tiles.indexOf(oldRobberTile);
