1443a4eeb24923908c54bdd9f2d1ecfc3a9fa464
RemcoGoy <remco.goy@hotmail.com>
2024-08-05 19:57:10 +0200

    Export to helper methods

--- src/app/game/moves.ts
+++ src/app/game/moves.ts
@@ -1,5 +1,5 @@
 import { GameState, SettleSpot } from "@/lib/types/game";
-import { isTopSettle } from "@/lib/utils";
+import { hasAdjecentRoads, hasAdjecentSettles, isTopSettle } from "@/lib/utils";
 import { Move } from "boardgame.io";
 import { INVALID_MOVE } from 'boardgame.io/core';
 
@@ -8,62 +8,7 @@ export const PlaceSettlement: Move<GameState> = ({ G, ctx }, coords: number[][])
     const playerID = ctx.currentPlayer;
 
     if (settleSpot && settleSpot.playerId === null) {
-        // Check adjecent settles
-        let hasAdjecentSettles = false;
-
-        const adjecentSettles = [];
-
-        if (isTopSettle(coords)) {
-            const topCoords = [[settleSpot.coords[2][0], settleSpot.coords[2][1] - 2], settleSpot.coords[0], settleSpot.coords[1]];
-            const topAdjecent: SettleSpot | undefined = G.settleSpots.find(spot => JSON.stringify(spot.coords) === JSON.stringify(topCoords));
-            if (topAdjecent) {
-                adjecentSettles.push(topAdjecent);
-            }
-
-            const leftCoords = [settleSpot.coords[0], [settleSpot.coords[1][0] - 1.5, settleSpot.coords[1][1] + 1], settleSpot.coords[2]];
-            const leftAdjecent: SettleSpot | undefined = G.settleSpots.find(spot => JSON.stringify(spot.coords) === JSON.stringify(leftCoords));;
-            if (leftAdjecent) {
-                adjecentSettles.push(leftAdjecent);
-            }
-
-            const rightCoords = [settleSpot.coords[1], settleSpot.coords[2], [settleSpot.coords[0][0] + 1.5, settleSpot.coords[0][1] + 1]]
-            const rightAdjecent: SettleSpot | undefined = G.settleSpots.find(spot => JSON.stringify(spot.coords) === JSON.stringify(rightCoords));;
-            if (rightAdjecent) {
-                adjecentSettles.push(rightAdjecent);
-            }
-        } else {
-            const leftCoords = [[settleSpot.coords[2][0] - 1.5, settleSpot.coords[2][1] - 1], settleSpot.coords[0], settleSpot.coords[1]];
-            const leftAdjecent: SettleSpot | undefined = G.settleSpots.find(spot => JSON.stringify(spot.coords) === JSON.stringify(leftCoords));;
-            if (leftAdjecent) {
-                adjecentSettles.push(leftAdjecent);
-            }
-
-            const rightCoords = [settleSpot.coords[0], [settleSpot.coords[1][0] + 1.5, settleSpot.coords[1][1] - 1], settleSpot.coords[2]];
-            const rightAdjecent: SettleSpot | undefined = G.settleSpots.find(spot => JSON.stringify(spot.coords) === JSON.stringify(rightCoords));;
-            if (rightAdjecent) {
-                adjecentSettles.push(rightAdjecent);
-            }
-
-            const bottomCoords = [settleSpot.coords[1], settleSpot.coords[2], [settleSpot.coords[0][0], settleSpot.coords[0][1] + 2]];
-            const bottomAdjecent: SettleSpot | undefined = G.settleSpots.find(spot => JSON.stringify(spot.coords) === JSON.stringify(bottomCoords));;
-            if (bottomAdjecent) {
-                adjecentSettles.push(bottomAdjecent);
-            }
-        }
-
-        if (adjecentSettles.some(s => s.playerId !== null)) {
-            hasAdjecentSettles = true;
-        }
-
-        // Check adjecent roads
-        let hasAdjecentRoads = false;
-
-        const adjecentRoads = G.roads.filter(r => JSON.stringify(settleSpot.coords) === JSON.stringify(r.coords[0]) || JSON.stringify(settleSpot.coords) === JSON.stringify(r.coords[1]));
-        if (adjecentRoads.some(r => r.playerId === playerID)) {
-            hasAdjecentRoads = true;
-        }
-
-        if (!hasAdjecentSettles && hasAdjecentRoads) {
+        if (!hasAdjecentSettles(G, settleSpot) && hasAdjecentRoads(G, settleSpot, playerID)) {
             settleSpot.playerId = playerID;
             const player = G.players.find(p => p.id === playerID);
             if (player) {
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { GameState, SettleSpot } from "./types/game";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -7,4 +8,65 @@ export function cn(...inputs: ClassValue[]) {
 
 export function isTopSettle(coords: number[][]) {
   return coords[0][1] === coords[1][1];
+}
+
+export function hasAdjecentRoads(G: GameState, settleSpot: SettleSpot, currentPlayer: string) {
+  let hasAdjecentRoads = false;
+
+  const adjecentRoads = G.roads.filter(r => JSON.stringify(settleSpot.coords) === JSON.stringify(r.coords[0]) || JSON.stringify(settleSpot.coords) === JSON.stringify(r.coords[1]));
+  if (adjecentRoads.some(r => r.playerId === currentPlayer)) {
+    hasAdjecentRoads = true;
+  }
+
+  return hasAdjecentRoads;
+}
+
+export function hasAdjecentSettles(G: GameState, settleSpot: SettleSpot) {
+  let hasAdjecentSettles = false;
+
+  const adjecentSettles = [];
+
+  if (isTopSettle(settleSpot.coords)) {
+    const topCoords = [[settleSpot.coords[2][0], settleSpot.coords[2][1] - 2], settleSpot.coords[0], settleSpot.coords[1]];
+    const topAdjecent: SettleSpot | undefined = G.settleSpots.find(spot => JSON.stringify(spot.coords) === JSON.stringify(topCoords));
+    if (topAdjecent) {
+      adjecentSettles.push(topAdjecent);
+    }
+
+    const leftCoords = [settleSpot.coords[0], [settleSpot.coords[1][0] - 1.5, settleSpot.coords[1][1] + 1], settleSpot.coords[2]];
+    const leftAdjecent: SettleSpot | undefined = G.settleSpots.find(spot => JSON.stringify(spot.coords) === JSON.stringify(leftCoords));;
+    if (leftAdjecent) {
+      adjecentSettles.push(leftAdjecent);
+    }
+
+    const rightCoords = [settleSpot.coords[1], settleSpot.coords[2], [settleSpot.coords[0][0] + 1.5, settleSpot.coords[0][1] + 1]]
+    const rightAdjecent: SettleSpot | undefined = G.settleSpots.find(spot => JSON.stringify(spot.coords) === JSON.stringify(rightCoords));;
+    if (rightAdjecent) {
+      adjecentSettles.push(rightAdjecent);
+    }
+  } else {
+    const leftCoords = [[settleSpot.coords[2][0] - 1.5, settleSpot.coords[2][1] - 1], settleSpot.coords[0], settleSpot.coords[1]];
+    const leftAdjecent: SettleSpot | undefined = G.settleSpots.find(spot => JSON.stringify(spot.coords) === JSON.stringify(leftCoords));;
+    if (leftAdjecent) {
+      adjecentSettles.push(leftAdjecent);
+    }
+
+    const rightCoords = [settleSpot.coords[0], [settleSpot.coords[1][0] + 1.5, settleSpot.coords[1][1] - 1], settleSpot.coords[2]];
+    const rightAdjecent: SettleSpot | undefined = G.settleSpots.find(spot => JSON.stringify(spot.coords) === JSON.stringify(rightCoords));;
+    if (rightAdjecent) {
+      adjecentSettles.push(rightAdjecent);
+    }
+
+    const bottomCoords = [settleSpot.coords[1], settleSpot.coords[2], [settleSpot.coords[0][0], settleSpot.coords[0][1] + 2]];
+    const bottomAdjecent: SettleSpot | undefined = G.settleSpots.find(spot => JSON.stringify(spot.coords) === JSON.stringify(bottomCoords));;
+    if (bottomAdjecent) {
+      adjecentSettles.push(bottomAdjecent);
+    }
+  }
+
+  if (adjecentSettles.some(s => s.playerId !== null)) {
+    hasAdjecentSettles = true;
+  }
+
+  return hasAdjecentSettles;
 }
